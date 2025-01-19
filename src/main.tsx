035ce12b67af28e41e1ddcd4c85ffe132254e821
Breno LV <beniolimavasc@gmail.com>
2025-01-18 23:58:55 -0300

    ajustes

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 // src/main.tsx
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import App from './App'; // Contém todas as páginas
-import HomePage from './pages/HomePage';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import App from "./App";
+import HomePage from "./pages/HomePage";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,5 +15,5 @@ ReactDOM.render(
       </Routes>
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
